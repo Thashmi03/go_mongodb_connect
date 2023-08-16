fd16392e3c87ab1ce53cfc0c09dad3b6f14596c5
Thashmi03 <thashmigaa.cs20@bitsathy.ac.in>
2023-08-16 17:59:18 +0530

    .

--- services/res.go
+++ services/res.go
@@ -10,28 +10,28 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func FindRes()([]*models.Res,error){
-	ctx,_:=context.WithTimeout(context.Background(),10*time.Second)
-	filter:=bson.D{}
-	result,err := ProductContext().Find(ctx,filter,options.Find().SetLimit(10))
-	if err!=nil{
+func FindRes() ([]*models.Res, error) {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	filter := bson.D{}
+	result, err := ProductContext().Find(ctx, filter, options.Find().SetLimit(10))
+	if err != nil {
 		fmt.Println(err.Error())
-		return nil,err
-	}else{
-		var rest[]*models.Res
-		for result.Next(ctx){
-			post:=&models.Res{}
-			err:=result.Decode(post)
-			if err!=nil{
-				return nil,err
+		return nil, err
+	} else {
+		var rest []*models.Res
+		for result.Next(ctx) {
+			post := &models.Res{}
+			err := result.Decode(post)
+			if err != nil {
+				return nil, err
 			}
-			
-			rest=append(rest, post)
+
+			rest = append(rest, post)
 		}
-		if err:=result.Err();err!=nil{
-			return nil,err
+		if err := result.Err(); err != nil {
+			return nil, err
 		}
-		return rest,nil
+		return rest, nil
 	}
-	
+
 }
